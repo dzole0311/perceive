12c9dcc56fed8f11647ee8e78844ff8597d9c430
dzole0311 <dzole0311@gmail.com>
2022-04-03 17:14:43 +0200

    Utils tests

---
+++ front-end/src/app/shared/utils.spec.ts
@@ -0,0 +1,14 @@
+import { formatTime, formatBytes } from './utils';
+
+it('should format the seconds to a valid time string', () => {
+  expect(formatTime(1)).toBe('1s');
+  expect(formatTime(1107)).toBe('18mins');
+  expect(formatTime(4800)).toBe('1.3h');
+});
+
+it('should format the bytes input to a correct memory unit', () => {
+  expect(formatBytes(1456)).toBe(1.42);
+  expect(formatBytes(10456)).toBe(10.21);
+  expect(formatBytes(-2424)).toBe(0);
+  expect(formatBytes(NaN)).toBe(0);
+})
--- front-end/src/app/shared/utils.ts
+++ front-end/src/app/shared/utils.ts
@@ -30,5 +30,6 @@ export const formatBytes = (bytes: number, decimals = 2) => {
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
   const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+  const result = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+  return result ? result : 0;
 }
