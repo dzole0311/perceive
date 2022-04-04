9bd6268739e7aeef293fe2bada1aa4beffb7452c
dzole0311 <dzole0311@gmail.com>
2022-04-05 01:27:46 +0200

    Fixed mock

--- front-end/src/app/home/components/home.component.spec.ts
+++ front-end/src/app/home/components/home.component.spec.ts
@@ -13,9 +13,30 @@ import {PlatformComponent} from "./system-overview/platform/platform.component";
 import {UptimeComponent} from "./system-overview/uptime/uptime.component";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {HighchartsChartModule} from "highcharts-angular";
+import {WebsocketApiService} from "../../shared/services/websocket-api.service";
+import {BehaviorSubject, Subject} from "rxjs";
+import {CpuLoadPayload} from "../../shared/interfaces/interfaces";
 
 // Mock of the ToastService, no need to be provided to the component
 function mockToastService() {}
+
+const subjectMock = new Subject();
+const cpuPayloadMock = new BehaviorSubject<CpuLoadPayload>({
+  timeSeries: [[0, 0]],
+  systemOverview: {
+    platform: 'Loading...',
+    uptime: 0,
+    cpuCount: 0,
+    freeMemory: 0,
+    totalMemory: 0
+  }
+});
+
+// Mock the Websocket connection
+const websocketApiMock = {
+  cpuPayload: cpuPayloadMock.asObservable(),
+  subject: subjectMock.asObservable()
+}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -36,7 +57,10 @@ describe('HomeComponent', () => {
         PlatformComponent,
         UptimeComponent
       ],
-      providers: [ {provide: ToastrService, useValue: mockToastService } ]
+      providers: [
+        { provide: ToastrService, useValue: mockToastService },
+        { provide: WebsocketApiService, useValue: websocketApiMock }
+      ]
     })
       .compileComponents();
   });
